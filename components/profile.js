da3873e414c86ddf2e9c9a9d6db4f9ec594a2dcc
Rob Brenan <rbrenan@gmail.com>
2023-03-14 21:44:17 -0400

    Layout work

---
+++ components/profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Profile({ userContext }) {
+  
+  if (userContext.isLoading) return <div>Loading...</div>;
+  if (userContext.error) return <div>{error.message}</div>;
+
+  return (
+    userContext.user && (
+      <div>
+        <img src={userContext.user.picture} alt={userContext.user.name} />
+        <h2>{userContext.user.name}</h2>
+        <p>{userContext.user.email}</p>
+      </div>
+    )
+  );
+}
