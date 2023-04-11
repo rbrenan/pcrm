15d7a79b6f681bb6d3ff34d102e0733a0df4a156
Rob Brenan <rbrenan@gmail.com>
2023-04-11 19:51:48 -0400

    Added person delete and person add dialog

--- pages/api/people/[personId].js
+++ pages/api/people/[personId].js
@@ -1,9 +1,9 @@
-import { updatePerson } from "../../../lib/prisma";
+import { deletePerson, updatePerson } from "../../../lib/prisma";
 import { withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function handler(req, res) {
   console.log("persondId route");
-
+  console.log(JSON.stringify(req.query));
   if (req.method === "PUT") {
     //console.log("BODY: " + JSON.stringify(req.body));
     // Validate the person object
@@ -14,5 +14,14 @@ export default withApiAuthRequired(async function handler(req, res) {
     //console.log("about to update person");
     const modifiedPerson = await updatePerson(req.body);
     return res.status(200).json({ data: modifiedPerson });
+  } else if (req.query.delete === "1") {
+    console.log("deleting");
+    await deletePerson(req.query.personId);
+    return (
+      res
+        /*.status(200)
+      .json({ data: `Deleted person ${req.query.personId}` })*/
+        .redirect("/people")
+    );
   }
 });
--- pages/people.jsx
+++ pages/people.jsx
@@ -4,6 +4,22 @@ import React, { useReducer, useState } from "react";
 import { getPeople, getPrefixes, getSuffixes } from "../lib/prisma";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { DataGrid } from "@mui/x-data-grid";
+import {
+  TextField,
+  Button,
+  Container,
+  Stack,
+  Select,
+  MenuItem,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  InputLabel,
+} from "@mui/material";
+import { FormatBold } from "@mui/icons-material";
 
 const formReducer = (state, event) => {
   if (event.reset) {
@@ -27,8 +43,28 @@ export default function People(props) {
 
   const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
+  const [openPopup, setOpenPopup] = useState(false);
 
-  const handleSubmit = (event) => {
+  const router = useRouter();
+  // Call this function whenever you want to
+  // refresh props!
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await fetch(`/api/people/`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    refreshData();
+    setOpenPopup(false);
+  };
+  /*const handleSubmit = (event) => {
     event.preventDefault();
     setSubmitting(true);
 
@@ -45,6 +81,7 @@ export default function People(props) {
       });
     }, 3000);
   };
+*/
 
   const handleChange = (event) => {
     setFormData({
@@ -53,91 +90,217 @@ export default function People(props) {
     });
   };
 
+  const peopleRows = props.people.map((person) => ({
+    id: person.personId,
+    prefix: person.prefix?.prefix,
+    firstName: person.firstName,
+    middleName: person.middleName,
+    lastName: person.lastName,
+    suffix: person.suffix?.suffix,
+    edit: person.personId,
+    delete: person.personId,
+  }));
+
+  const peopleCols = [
+    {
+      field: "prefix",
+      headerName: "Prefix",
+      width: 70,
+    },
+    {
+      field: "firstName",
+      headerName: "First Name",
+      width: 150,
+    },
+    {
+      field: "middleName",
+      headerName: "Middle Name",
+      width: 150,
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
+      width: 150,
+    },
+    {
+      field: "suffix",
+      headerName: "Suffix",
+      width: 70,
+    },
+    {
+      field: "edit",
+      headerName: "Edit",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Link href={`/people/${encodeURIComponent(params.value)}`} passHref>
+            <Button>Edit</Button>
+          </Link>
+        );
+      },
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Link
+            href={{
+              pathname: `/api/people/${encodeURIComponent(params.value)}`,
+              query: { delete: 1 },
+            }}
+            passHref
+          >
+            <Button>Delete</Button>
+          </Link>
+        );
+      },
+    },
+  ];
+
   return (
     <Layout userContext={userContext}>
       <div>
-        <h1>Add person</h1>
-        {submitting ? (
-          <div>Submitting...</div>
-        ) : (
-          /*<form onSubmit={handleSubmit}>*/
-          <form method="POST" action="/api/people/">
-            <fieldset>
-              <label>
-                <p>Prefix</p>
-                {/*<input
-                  name="prefix"
-                  value={formData.prefix || ""}
-                  list="prefixes"
-                  onChange={handleChange}
-                />
-                <datalist id="prefixes">
-                  {props.prefixes.map((prefix) => (
-                    <option key={prefix.prefixId} value={prefix.prefix} />
-                  ))}
-                  </datalist>*/}
-                <select name="prefixId">
-                  <option value="">--Prefix--</option>
-                  {props.prefixes.map((prefix) => (
-                    <option key={prefix.prefixId} value={prefix.prefixId}>
-                      {prefix.prefix}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <label>
-                <p>First name</p>
-                <input
-                  name="firstName"
-                  value={formData.firstName || ""}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                <p>Middle name</p>
-                <input
-                  name="middleName"
-                  value={formData.middleName || ""}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                <p>Last name</p>
-                <input
-                  name="lastName"
-                  value={formData.lastName || ""}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                <p>Suffix</p>
-                <select name="suffixId">
-                  <option value="">--Suffix--</option>
-                  {props.suffixes.map((suffix) => (
-                    <option key={suffix.suffixId} value={suffix.suffixId}>
-                      {suffix.suffix}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </fieldset>
-            <button type="submit">Submit</button>
-          </form>
-        )}
+        {/*
+        <Typography variant="h5" fontWeight="bold" gutterBottom>
+          Add person
+  </Typography>*/}
+        <Button
+          onClick={() => {
+            setOpenPopup(true);
+          }}
+        >
+          Add Person
+        </Button>
         <br />
-        <h1>People</h1>
-        <ul>
-          {props.people.map((person) => (
-            <li key={person.personId}>
-              <Link href={`/people/${encodeURIComponent(person.personId)}`}>
-                {/*JSON.stringify(person)*/} {person.prefix?.prefix}{" "}
-                {person.firstName} {person.middleName} {person.lastName}{" "}
-                {person.suffix?.suffix}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <Typography variant="h5" fontWeight="bold" gutterBottom>
+          People
+        </Typography>
+        <div style={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={peopleRows}
+            columns={peopleCols}
+            checkboxSelection
+            density="compact"
+            sx={{
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: "secondary",
+                color: "secondary",
+                fontSize: 16,
+                fontWeight: "bold",
+              },
+            }}
+          />
+        </div>
       </div>
+      <Dialog open={openPopup}>
+        <DialogTitle>Add Person</DialogTitle>
+        <DialogContent>
+          <form method="POST" onSubmit={handleSubmit}>
+            <fieldset>
+              <Stack
+                spacing={2}
+                direction="row"
+                sx={{ marginBottom: 4, marginTop: 4 }}
+              >
+                <InputLabel id="prefix-label">Prefix</InputLabel>
+                <Select
+                  name="prefixId"
+                  value={formData.prefixId}
+                  label="Prefix"
+                  labelId="prefix-label"
+                  onChange={handleChange}
+                  sx={{ mb: 4 }}
+                >
+                  <MenuItem key="" value="">
+                    ---Prefix---
+                  </MenuItem>
+                  {props.prefixes?.map((prefix) => (
+                    <MenuItem key={prefix.prefixId} value={prefix.prefixId}>
+                      {prefix.prefix}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Stack>
+              <TextField
+                name="firstName"
+                type="text"
+                variant="outlined"
+                color="secondary"
+                label="First Name"
+                width="30%"
+                onChange={handleChange}
+                value={formData.firstName}
+                fullWidth
+                required
+                sx={{ mb: 4 }}
+              />
+              <TextField
+                name="middleName"
+                type="text"
+                variant="outlined"
+                color="secondary"
+                label="Middle Name"
+                onChange={handleChange}
+                value={formData.middleName}
+                fullWidth
+                sx={{ mb: 4, width: 500 }}
+              />
+              <TextField
+                name="lastName"
+                type="text"
+                variant="outlined"
+                color="secondary"
+                label="Last Name"
+                width="30%"
+                onChange={handleChange}
+                value={formData.lastName}
+                fullWidth
+                required
+                sx={{ mb: 4 }}
+              />
+              <Stack
+                spacing={2}
+                direction="row"
+                sx={{ marginBottom: 4, marginTop: 4 }}
+              >
+                <InputLabel id="suffix-label">Suffix</InputLabel>
+                <Select
+                  name="suffixId"
+                  value={formData.suffixId}
+                  label="Suffix"
+                  labelId="suffix-label"
+                  width="10%"
+                  onChange={handleChange}
+                  sx={{ mb: 4 }}
+                >
+                  <MenuItem key="" value="">
+                    ---Suffix---
+                  </MenuItem>
+                  {props.suffixes?.map((suffix) => (
+                    <MenuItem key={suffix.suffixId} value={suffix.suffixId}>
+                      {suffix.suffix}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Stack>
+            </fieldset>
+            <Button type="submit" variant="contained" sx={{ m: 2 }}>
+              Submit
+            </Button>
+          </form>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setOpenPopup(false);
+            }}
+            sx={{ m: 2 }}
+          >
+            Cancel
+          </Button>
+        </DialogContent>
+      </Dialog>
     </Layout>
   );
 }
